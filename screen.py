d3d77ab70d43caf8e77e9bb1094e83938100cf1f
Nikk <c3362623@uon.edu.au>
2024-11-25 13:16:50 +1100

    Changed Background
    Now uses the created background image.
    Changed the initial Y-Axis of the user to align with the map

--- screen.py
+++ screen.py
@@ -4,8 +4,14 @@ from player import Player
 # Initialize the pygame
 pygame.init()
 
+# Set the window title
+pygame.display.set_caption('Zombie Apocalypse')
+
 # Defining a backgound color
 background_color = (0, 0, 255)
+
+# Load the background image
+background = pygame.image.load('background.png')
 
 # Define the screen dimensions 
 # Screen object (width, height)
@@ -14,7 +20,7 @@ screen = pygame.display.set_mode((800, 600))
 # 'pygame.display.flip()' updates the display
 
 # Create a player object
-player = Player('player.png', 800, 600) # Currently hardcoded
+player = Player('player.png', 800, 750) # Currently hardcoded
 
 # Boolean variable to control the main loop
 running = True
@@ -31,8 +37,8 @@ while running:
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.move('right')
 
-    # Fill the screen with the background color
-    screen.fill(background_color)
+    # Using Blit you can draw the background image on the screen
+    screen.blit(background, (0, 0))
 
     # Create a player object
     player.draw(screen)
